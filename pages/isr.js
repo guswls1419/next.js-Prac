32b0f3fad60f687331ea631e5ce89182c04847a9
guswls1419 <guswns1419@naver.com>
2022-08-05 00:28:47 +0900

    Layout - Page

--- pages/isr.js
+++ pages/isr.js
@@ -1,11 +1,7 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-// ISR
-// 증분 정적 사이트를 재생성한다. => 특정주기로 데이터를 가져와서 다시 그려둔다.
-// SSR 과 같이 getStaticProps를 사용하고 revalidate라는 값을 리턴한다.
-// 즉, revalidate 값을 getStaticProps에서 리턴하면
-// revalidate(초 단위) 의 초 만큼 다시 데이터를 가져온다.
-// SSR과 SSG의 장점을 적절하게 사용할수 있다.
+import Layout from "../components/Layout";
+import SubLayout from "../components/SuLayout";
 
 export function getStaticProps() {
   console.log("server");
@@ -17,171 +13,38 @@ export function getStaticProps() {
 
 export default function ISR({ time }) {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">{time}</h1>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    <>
+      <h1 className="title">
+        <span style={{ fontWeight: "bold", fontSize: "30px" }}>예시👉 </span>
+        {time}
+      </h1>
+      <div>(새로고침 하면서 client, server 양쪽 console확인)</div>
+      <br />
+      <br />
+      <div>
+        <span style={{ fontWeight: "bold", fontSize: "40px" }}>ISR</span>
+        <br />
+        <br />
+        증분 정적 사이트를 재생성한다. ➡ 특정주기로 데이터를 가져와서 다시
+        그려둔다.
+        <br />
+        SSR 과 같이 getStaticProps를 사용하고 revalidate라는 값을 리턴한다.
+        <br />
+        <br />
+        즉, revalidate 값을 getStaticProps에서 리턴하면
+        <br />
+        revalidate(초 단위) 의 초 만큼 다시 데이터를 가져온다.
+        <br />
+        <br />
+        SSR과 SSG의 장점을 적절하게 사용할수 있다.
+      </div>
+    </>
   );
 }
+ISR.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  );
+};
--- pages/ssg.js
+++ pages/ssg.js
@@ -1,21 +1,7 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-
-//SSG
-// 정적인 데이터를 가져와서 미리 그려둔다.
-// dev로 실행할 경우 SSG는 제대로 동작하지 않고 getServerSideProps 처럼 동작한다.
-//(dev는 개발환경으로 SSG는 개발환경에서 작동하지 않기 때문)
-// 제대로 보려면 build 후 'start'명령어로 실행시켜야한다.
-
-// 즉, build 할때 페이지를 만들어 버린다는 말.
-// ex)블로그 같이 정적인 페이지라고 할때 새로 배포하기 전까지 이전데이터가 유지되어 이전데이터 화면을 보여주는것.
-
-// SSG는 정적인 페이지에 한해서 이미 만들어진 페이지를 만들어 버리기때문에 서버 부하가 적다.
-
-//<사용 함수>
-// getStaticProps : 미리 정적인 화면을 그려둔다.
-// getStaticPaths : 어떤 path를 써야할지 알기위해 사용한다. ex) [ooo].js 와 같이 어떤 path가 올지 모를때 사용한다.
-// => getStaticPaths를 쓸때는 꼭 getStaticProps를 같이 써야한다.
+import Layout from "../components/Layout";
+import SubLayout from "../components/SuLayout";
 
 export function getStaticProps() {
   console.log("server");
@@ -23,173 +9,50 @@ export function getStaticProps() {
     props: { time: new Date().toISOString() },
   };
 }
+
 export default function SSG({ time }) {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">{time}</h1>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    <>
+      <h1 className="title">
+        <span style={{ fontWeight: "bold", fontSize: "30px" }}>예시👉 </span>
+        {time}
+      </h1>
+      <div>(새로고침 하면서 client, server 양쪽 console확인)</div>
+      <br />
+      <br />
+      <div>
+        <span style={{ fontWeight: "bold", fontSize: "40px" }}>SSG</span>
+        <br />
+        <br />
+        SSG 정적인 데이터를 가져와서 미리 그려둔다. <br />
+        <br />
+        dev로 실행할 경우 SSG는 제대로 동작하지 않고
+        <br /> getServerSideProps 처럼 동작한다.
+        <br /> (dev는 개발환경으로 SSG는 개발환경에서 작동하지 않기 때문) <br />
+        제대로 보려면 build 후 'start'명령어로 실행시켜야한다. <br />
+        <br />
+        즉, build 할때 페이지를 만들어 버린다는 말. <br />
+        (ex)블로그 같이 정적인 페이지라고 할때 새로 배포하기 전까지 <br />
+        이전데이터가 유지되어 이전데이터 화면을 보여주는것. <br />
+        <br />
+        SSG는 정적인 페이지에 한해서 이미 만들어진 페이지를 만들어
+        <br /> 버리기때문에 서버 부하가 적다.
+        <br />
+        <br /> 사용 함수 <br />
+        getStaticProps : 미리 정적인 화면을 그려둔다.
+        <br />
+        getStaticPaths : 어떤 path를 써야할지 알기위해 사용한다. <br />
+        (ex)[ooo].js 와 같이 어떤 path가 올지 모를때 사용한다.
+        <br />
+        <br /> *getStaticPaths를 쓸때는 꼭 getStaticProps를 같이 써야한다.
+      </div>
+    </>
   );
 }
+SSG.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <SubLayout>{page}</SubLayout>
+    </Layout>
+  );
+};
